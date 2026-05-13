7165a9460bee3b1d0e17d85955ba3a505cc06758
Gogo <gbrys4046@gmail.com>
2026-05-13 10:41:46 +0200

    Fix: API PATH

--- backend/mealshare-server/db.js
+++ backend/mealshare-server/db.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQL_DATABASE || 'mealshare_db',
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    // ⚠️ هذا هو الجزء الحاسم للاتصال بـ Railway
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
