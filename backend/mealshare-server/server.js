89f9c1a3e251e3cca47594bc22b70861900967cc
Gogo <gbrys4046@gmail.com>
2026-05-13 10:06:15 +0200

    Fix: API PATH

--- backend/mealshare-server/server.js
+++ backend/mealshare-server/server.js
@@ -34,7 +34,8 @@ const corsOptions = {
       'http://127.0.0.1:3000',
       'http://localhost:3000',
       'http://127.0.0.1:3001',
-      'http://localhost:3001'
+      'http://localhost:3001',
+      'https://mealshare.onrender.com'
     ];
 
     if (allowedOrigins.includes(origin)) {
